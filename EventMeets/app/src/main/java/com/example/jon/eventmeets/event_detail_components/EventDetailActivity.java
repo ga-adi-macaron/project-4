1a3e2eb44a75073510f9be1615fda8a6c6704489
Jon Lieblich <jlieblich1@gmail.com>
2016-12-20 17:20:16 -0500

    added some basic layouts

---
+++ EventMeets/app/src/main/java/com/example/jon/eventmeets/event_detail_components/EventDetailActivity.java
@@ -0,0 +1,32 @@
+package com.example.jon.eventmeets.event_detail_components;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.jon.eventmeets.R;
+
+public class EventDetailActivity extends AppCompatActivity {
+    private String mEventId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EventDetailActivity.this, EventMessageActivity.class);
+                intent.putExtra("event id", mEventId);
+            }
+        });
+    }
+}
---
+++ EventMeets/app/src/main/java/com/example/jon/eventmeets/event_detail_components/EventMessageActivity.java
@@ -0,0 +1,15 @@
+package com.example.jon.eventmeets.event_detail_components;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jon.eventmeets.R;
+
+public class EventMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_message);
+    }
+}
