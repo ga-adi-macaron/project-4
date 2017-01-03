8fda94df4f938eb5ee403e497dd12d0ba78f3808
Jon Lieblich <jlieblich1@gmail.com>
2017-01-02 22:23:49 -0500

    merged

--- EventMeets/app/src/main/java/com/example/jon/eventmeets/event_detail_components/EventMessageActivity.java
+++ EventMeets/app/src/main/java/com/example/jon/eventmeets/event_detail_components/EventMessageActivity.java
@@ -4,15 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.example.jon.eventmeets.R;
-import com.example.jon.eventmeets.model.BaseUser;
 import com.example.jon.eventmeets.model.DatabaseGameObject;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
