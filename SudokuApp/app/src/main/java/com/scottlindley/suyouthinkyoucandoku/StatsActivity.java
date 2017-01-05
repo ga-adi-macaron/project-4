6616ad861a2b0f92868b3f9de5a36276fac532c2
ScottLindley <scottdlindley@gmail.com>
2017-01-05 09:44:31 -0500

    clicking stats card now finishes activity

--- SudokuApp/app/src/main/java/com/scottlindley/suyouthinkyoucandoku/StatsActivity.java
+++ SudokuApp/app/src/main/java/com/scottlindley/suyouthinkyoucandoku/StatsActivity.java
@@ -3,6 +3,7 @@ package com.scottlindley.suyouthinkyoucandoku;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateUtils;
+import android.view.View;
 import android.widget.TextView;
 
 public class StatsActivity extends AppCompatActivity {
@@ -31,6 +32,17 @@ public class StatsActivity extends AppCompatActivity {
         mBestTime.setText("Best Time: "+ time);
         mRacesWon.setText("Races Won:  "+ stats.getRacesWon());
         mRacesLost.setText("Races Lost: "+ stats.getRacesLost());
+
+        findViewById(R.id.stats_card).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mHighScore.setVisibility(View.INVISIBLE);
+                mBestTime.setVisibility(View.INVISIBLE);
+                mRacesWon.setVisibility(View.INVISIBLE);
+                mRacesLost.setVisibility(View.INVISIBLE);
+                finishAfterTransition();
+            }
+        });
     }
 
     @Override
